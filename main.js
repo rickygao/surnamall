da8eca46d218e85ce42f86b225cda90737431ec4
rickygao <rckgao@gmail.com>
2022-02-22 18:41:16 +0800

    Neat

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as d3 from 'd3'
 import 'd3-transition'
-
 import 'modern-normalize/modern-normalize.css'
+
 import './style.scss'
 import data from './data.json'
 
@@ -22,14 +22,12 @@ container
 container.selectAll('div:not(.banner-container)').data(data).join(
     (enter) => {
         const itemContainer = enter.append('div').classed('item-container', true)
-        itemContainer.on('click', (event) => {
-            event.currentTarget.classList.toggle('clicked')
-        })
+        itemContainer.on('click', e => e.currentTarget.classList.toggle('clicked'))
 
         itemContainer.append('div').classed('index', true).text((_, i) => i + 1)
 
         const item = itemContainer.append('div').classed('item', true).call(fadeIn)
         item.append('p').classed('pinyin', true).text(d => d.pinyin)
         item.append('h1').classed('name', true).text(d => d.name)
-    },
+    }
 )
